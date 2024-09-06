bf61cd0a8da6387d0f15adf6831cd016594dc914
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-09-06 11:45:58 -0500

    computer outputting random choices

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice(){
+    const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
+}
+console.log(getComputerChoice());
